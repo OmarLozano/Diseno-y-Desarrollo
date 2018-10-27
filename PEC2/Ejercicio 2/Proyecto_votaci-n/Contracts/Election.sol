efcbde6511308ac827dfa3eb9af5560cf304c4fa
OmarLozano <35610694+OmarLozano@users.noreply.github.com>
2018-10-28 00:23:18 +0200

    Add files via upload

---
+++ PEC2/Ejercicio 2/Proyecto_votaci-n/Contracts/Election.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.4.2;
+
+contract Election {
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+    //store acounts that have voted
+    mapping(address => bool) public voters;
+    // Store Candidates
+    // Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+    // Store Candidates Count
+    uint public candidatesCount;
+event votedEvent (
+    uint indexed _candidateId
+);
+    constructor () public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+    function vote (uint _candidateId) public {
+        //require that they haven voted before
+        require(!voters[msg.sender]);
+
+        //require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);        
+        //record that voter has voted
+        
+        voters[msg.sender] = true;
+
+
+        //update candidate vote count
+        candidates[_candidateId].voteCount ++;
+    }
+}
